329de2e9cf660d4f2aca8b0811bb483c970a59d2
kwangin.jung <inylove82@gmail.com>
2019-09-09 17:04:23 +0900

    Add client part

---
+++ client/src/components/ListItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="list-item">
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ListItem extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.list-item {
+  border-radius: 4px;
+  margin: 5px;
+  background-color: #ffffff;
+}
+</style>
---
+++ client/src/views/Main.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <div class="tweet-list">
+      <ListItem msg="List" />
+      <ListItem msg="List" />
+      <ListItem msg="List" />
+      <ListItem msg="List" />
+      <ListItem msg="List" />
+    </div>
+    <div class="tweet-list">
+      <ListItem msg="List" />
+      <ListItem msg="List" />
+      <ListItem msg="List" />
+      <ListItem msg="List" />
+      <ListItem msg="List" />
+    </div>
+    <div class="tweet-list">
+      <div class="list-item">List</div>
+      <div class="list-item">List</div>
+      <div class="list-item">List</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import ListItem from "@/components/ListItem.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    ListItem
+  }
+})
+export default class Main extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: flex;
+  .tweet-list {
+    margin: 0 10px;
+    width: 30%;
+    background-color: #eeeeee;
+  }
+}
+</style>
