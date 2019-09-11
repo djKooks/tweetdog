6e3bc4bbc02fa1318c44ac889c70c4e30ca94cc0
kwangin.jung <inylove82@gmail.com>
2019-09-11 14:30:19 +0900

    Change db table

--- server/db.py
+++ server/db.py
@@ -1,7 +1,12 @@
 from sqlalchemy import (
-    MetaData, Table, Column, ForeignKey,
-    Integer, String, Date
+    create_engine, MetaData, Table, Column, ForeignKey,
+    Integer, String, DateTime, 
 )
+
+from .settings import config
+
+
+DB_URL = "mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
 meta = MetaData()
 
@@ -9,5 +14,18 @@ tweets = Table(
     'tweets', meta,
     Column('id', Integer, primary_key=True),
     Column('tweet_text', String(512), nullable=False),
-    Column('pub_date', Date, nullable=False)
+    Column('pub_date', DateTime, nullable=False)
 )
+
+db_url = DB_URL.format(**config['mysql'])
+engine = create_engine(db_url)
+
+
+def update_tweet(tweet_text, pub_date):
+    conn = engine.connect()
+    conn.execute(
+        tweets.insert().values(
+            tweet_text=tweet_text,
+            pub_date=pub_date
+        )
+    )
