bb36ee57107c6bfb8728f8db400d794bbafff323
kwangin.jung <inylove82@gmail.com>
2019-10-27 10:34:13 +0900

    Render data to page

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="home">
     <div class="tweet-list">
-      <ListItem msg="List" />
-      <ListItem msg="List" />
-      <ListItem msg="List" />
-      <ListItem msg="List" />
-      <ListItem msg="List" />
+      <ListItem v-for="tweet in this.tweets" :key="tweet.id" :msg="tweet.tweet_text" />
     </div>
     <div class="tweet-list">
       <ListItem msg="List" />
@@ -23,7 +19,9 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { State, Action, Getter } from 'vuex-class'
+
 import ListItem from "@/components/ListItem.vue"; // @ is an alias to /src
 
 @Component({
@@ -31,7 +29,21 @@ import ListItem from "@/components/ListItem.vue"; // @ is an alias to /src
     ListItem
   }
 })
-export default class Main extends Vue {}
+export default class Main extends Vue {
+  @Action('getRecentTweets') getRecentTweets: any
+  @Getter('tweets') tweets: any
+
+  mounted() {
+    console.log('mounted!')
+    setInterval(() => {
+      this.getRecentTweets()
+      
+      this.tweets.forEach((twt: any) => {
+        console.log(twt)
+      })
+    }, 3000)
+  }
+}
 </script>
 
 <style scoped lang="scss">
