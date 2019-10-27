02de6514cfea2b0c64310df166cc69b71386328b
kwangin.jung <inylove82@gmail.com>
2019-10-27 10:45:31 +0900

    Setup store to fetch data

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,10 +1,44 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+interface State {
+  message: string
+  counter: number
+}
+
+const API_PREFIX: string = 'http://localhost:5000/api'
+const RECENT_TWEETS_REQUEST: string = API_PREFIX + '/recent-tweets'
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
-});
+  state: {
+    count: 0,
+    tweets: [],
+  },
+  getters: {
+    tweets(state) {
+      return state.tweets
+    },
+  },
+  mutations: {
+    updateTweets(state, payload) {
+      Vue.set(state, 'tweets', payload)
+    }
+  },
+  actions: {
+    async getRecentTweets({ commit, state }) {
+      axios
+        .get(RECENT_TWEETS_REQUEST)
+        .then((resp) => {
+          commit('updateTweets', resp.data)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+      
+    }
+  }
+})
